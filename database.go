7df343cc4c1b2a0df4b79b8f54055932a63c92ef
husada20 <husadahutasoit@gmail.com>
2023-08-01 23:51:12 +0700

    database in golang

---
+++ database.go
@@ -0,0 +1,21 @@
+package Database_go
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/golang_database")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Second)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
