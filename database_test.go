f89fb1d47280779d8d0989d7976ecf89569b6398
husada20 <husadahutasoit@gmail.com>
2023-08-01 23:35:47 +0700

    database in golang

---
+++ database_test.go
@@ -0,0 +1,24 @@
+package Database_go
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/golang_database")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// gunakan database
+
+	defer db.Close()
+
+}
