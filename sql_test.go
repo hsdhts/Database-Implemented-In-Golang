c35fdb8e881ea65769f754029924a2683c5f60a5
husada20 <husadahutasoit@gmail.com>
2023-08-05 13:58:49 +0700

    sql injection

--- sql_test.go
+++ sql_test.go
@@ -155,3 +155,32 @@ func TestQuerySqlComplex(t *testing.T) {
 		fmt.Println("created_at: ", created_at)
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	password := "admin123"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, script, username, password)
+
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Login berhasil", username)
+	} else {
+		fmt.Println("Gagal login")
+	}
+}
