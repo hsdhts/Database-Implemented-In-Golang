287788b41c7bb53e88597299ef223a34e2731c6f
husada20 <husadahutasoit@gmail.com>
2023-08-05 21:33:55 +0700

    updatedb transaction

--- sql_test.go
+++ sql_test.go
@@ -262,3 +262,39 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment Id", id)
 	}
 }
+
+// database transaction
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	script := "INSERT INTO  comments(email, comment) VALUES (?, ?)"
+	//db transaction
+	for i := 0; i < 10; i++ {
+		email := "batman" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Komentar ke" + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment Id", id)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		panic(err)
+	}
+}
