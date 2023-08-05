3870c3d1647adcb3de88564ab19624459fbd42dd
husada20 <husadahutasoit@gmail.com>
2023-08-05 21:23:52 +0700

    update

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -228,4 +229,36 @@ func TestAutoincrement(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println("Success insert new comment with id", insertId)
+}
+
+// prepare statement
+func TestPrepareStatement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO  comments(email, comment) VALUES (?, ?)"
+	statement, err := db.PrepareContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer statement.Close()
+
+	for i := 0; i < 10; i++ {
+		email := "batman" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Komentar ke" + strconv.Itoa(i)
+
+		result, err := statement.ExecContext(ctx, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment Id", id)
+	}
 }
